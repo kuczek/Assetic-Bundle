f6286251aef74dd105e44ef16dd588d37e1c44f3
Krystian Kuczek <krystian@hexmedia.pl>
2014-08-12 18:14:37 +0200

    Travis fix.

--- Tests/Utils/UrlReplacerTest.php
+++ Tests/Utils/UrlReplacerTest.php
@@ -84,7 +84,7 @@ class UrlReplacerTest extends \PHPUnit_Framework_TestCase
             array('@TestBundle/Resources/public/img/img.png', "../img/img.png", null, true),
             array('@@TestBundle/Resources/public/img/img.png', "vfs:///../web/bundles/img/img.png", null, true),
             array('@HexmediaTestBundle/Resources/public/img/img.png', "http://cdn.hexmedia.pl/bundles/hexmediatest/img/img.png", "http://cdn.hexmedia.pl/", true),
-            array('@@HexmediaTestBundle/Resources/public/img/img.png', "vfs:///../web/bundles/im-g/img.png", "http://cdn.hexmedia.pl/", true),
+            array('@@HexmediaTestBundle/Resources/public/img/img.png', "vfs:///../web/bundles/img/img.png", "http://cdn.hexmedia.pl/", true),
             array('@import "wp.pl";', '@import "wp.pl";', null, false),
             array('@@import "wp.pl";', '@@import "wp.pl";', null, false),
             array('@import "wp.pl";', '@import "wp.pl";', "http://cdn.hexmedia.pl/", false),
